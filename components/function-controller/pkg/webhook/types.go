9159238f1fa7d6ce4403af8b357f1c2f2331d0c0
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-01-09 10:31:19 +0100

    comment out injecting client

--- components/function-controller/pkg/webhook/types.go
+++ components/function-controller/pkg/webhook/types.go
@@ -3,11 +3,10 @@ package webhook
 import (
 	"github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
 	runtimeUtil "github.com/kyma-project/kyma/components/function-controller/pkg/utils"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type FunctionHandler struct {
 	v1alpha1.Function
-	Client      client.Client
+	// Client      client.Client
 	RuntimeInfo runtimeUtil.RuntimeInfo
 }
