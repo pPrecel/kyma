db2b7b84daedfd7263076d3c7821de7cad66fb03
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-01-10 11:29:19 +0100

    commit whatever I did not

--- components/function-controller/pkg/webhook/types.go
+++ components/function-controller/pkg/webhook/types.go
@@ -1,12 +1,1 @@
 package webhook
-
-import (
-	"github.com/kyma-project/kyma/components/function-controller/pkg/apis/serverless/v1alpha1"
-	runtimeUtil "github.com/kyma-project/kyma/components/function-controller/pkg/utils"
-)
-
-type FunctionHandler struct {
-	v1alpha1.Function
-	// Client      client.Client
-	RuntimeInfo runtimeUtil.RuntimeInfo
-}
--- components/function-controller/pkg/webhook/webhook.go
+++ components/function-controller/pkg/webhook/webhook.go
@@ -1,70 +1,37 @@
+/*
+Copyright 2019 The Kyma Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package webhook
 
 import (
-	"context"
-	"fmt"
-	"os"
-
-	runtimeUtil "github.com/kyma-project/kyma/components/function-controller/pkg/utils"
-	"github.com/pkg/errors"
-	corev1 "k8s.io/api/core/v1"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-// log is for logging in this package.
-var webhookLog = logf.Log.WithName("webhook")
+// AddToManagerFuncs is a list of functions to add all Controllers to the Manager
+var AddToManagerFuncs []func(manager.Manager) error
 
-var (
-	// name of function config
-	fnConfigName = getEnvDefault("CONTROLLER_CONFIGMAP", "fn-config")
-
-	// namespace of function config
-	fnConfigNamespace = getEnvDefault("CONTROLLER_CONFIGMAP_NS", "default")
-)
-
-func getEnvDefault(envName string, defaultValue string) string {
-	// use default value if environment variable is empty
-	var value string
-	if value = os.Getenv(envName); value == "" {
-		return defaultValue
+// AddToManager adds all Controllers to the Manager
+// +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations;validatingwebhookconfigurations,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+func AddToManager(m manager.Manager) error {
+	for _, f := range AddToManagerFuncs {
+		if err := f(m); err != nil {
+			return err
+		}
 	}
-	return value
-}
-
-func (r *FunctionHandler) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	var err error
-	rntInfo, err := getRuntimeInfo(mgr.GetClient(), fnConfigName, fnConfigNamespace)
-	if err != nil {
-		return err
-	}
-
-	r.RuntimeInfo = *rntInfo
-
-	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
-		Complete()
-}
-
-func getRuntimeInfo(cl client.Client, fnConfigName, fnConfigNamespace string) (*runtimeUtil.RuntimeInfo, error) {
-	cm := &corev1.ConfigMap{}
-
-	err := cl.Get(context.TODO(),
-		client.ObjectKey{
-			Name:      fnConfigName,
-			Namespace: fnConfigNamespace,
-		},
-		cm,
-	)
-
-	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("while reading ConfigMap %s from namespace %s", fnConfigName, fnConfigNamespace))
-	}
-
-	rnInfo, err := runtimeUtil.New(cm)
-	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("while creating RuntimeInfo from ConfigMap %s in namespace %s", cm.Name, cm.Namespace))
-	}
-	return rnInfo, nil
+	return nil
 }
